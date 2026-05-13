edd3b21a6f338c69c8e6002bc3aa45c71c6eeebe
luis Ortega <ortegaluiskb@gmail.com>
2026-05-13 17:57:56 -0600

    creacion de la bd y clases java

---
+++ src/main/java/com/pipecoding/ec/firtsapirest/model/entity/Cliente.java
@@ -0,0 +1,11 @@
+package com.pipecoding.ec.firtsapirest.model.entity;
+
+public class Cliente {
+
+    private Integer idCliente;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String fechaRegistro;
+
+}
---
+++ src/test/java/com/pipecoding/ec/firtsapirest/FirtsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipecoding.ec.firtsapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtsApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f71f6fc0540a3855e1bdc3e2fb10e42d23cab25c
luis Ortega <ortegaluiskb@gmail.com>
2026-05-13 19:50:11 -0600

    actualizamos el git Ignore y el controlador

--- src/main/java/com/pipecoding/ec/firtsapirest/controller/ClienteController.java
+++ src/main/java/com/pipecoding/ec/firtsapirest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.pipecoding.ec.firtsapirest.controller;
 import com.pipecoding.ec.firtsapirest.model.entity.Cliente;
 import com.pipecoding.ec.firtsapirest.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,22 +14,26 @@ public class ClienteController {
     private ICliente clienteService;
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @DeleteMapping("cliente")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
         Cliente clienteDelete = clienteService.findById(id);
         clienteService.delete(clienteDelete);
     }
 
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id) {
         return clienteService.findById(id);
     }
